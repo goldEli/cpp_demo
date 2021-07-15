063a1f76074c267d6882a7aed54918b81719ff9b
miaoyu <miaoyu2009@qq.com>
2021-07-15 10:14:01 +0800

    Initial Commit

---
+++ cpp_x/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_x
+//
+//  Created by 缪宇 on 2021/7/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
