de465ca9ba36311d182ca1f8ec44fb776035415a
miaoyu <miaoyu2009@qq.com>
2021-08-05 09:03:07 +0800

    feat: ✨ add menu

--- employee_anagement/worker_manager/worker_manager.cpp
+++ employee_anagement/worker_manager/worker_manager.cpp
@@ -25,5 +25,18 @@ WorkerManager::~WorkerManager() {
 }
 
 void WorkerManager::showMenu() {
-    cout << "menu" << endl;
+    cout << "*****************************************************" << endl;
+    cout << "******Welocme to the Employee Management System******" << endl;
+    cout << "*****************************************************" << endl;
+    cout << "1. Exit Management System" << endl;
+    cout << "2. Add employee information" << endl;
+    cout << "3. Show employee information" << endl;
+    cout << "4. Delete employees" << endl;
+    cout << "5. Modify employee information" << endl;
+    cout << "6. find employee information" << endl;
+    cout << "7. Sort by index" << endl;
+    cout << "8. Clear all document" << endl;
+    
+    
+    cout << "****************************************************" << endl;
 }
